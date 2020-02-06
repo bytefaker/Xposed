145d97073d365a959feac670efa524f2f632c9af
kenny <kennyallen0520@gmail.com>
2020-02-07 05:58:32 +0800

    rename

--- xposed.h
+++ xposed.h
@@ -3,23 +3,23 @@
 
 #include "xposed_shared.h"
 
-#define XPOSED_PROP_FILE "/system/xposed.prop"
+#define XPOSED_PROP_FILE "/system/shadow.prop"
 
 #if defined(__LP64__)
   #define XPOSED_LIB_DIR "/system/lib64/"
 #else
   #define XPOSED_LIB_DIR "/system/lib/"
 #endif
-#define XPOSED_LIB_DALVIK        XPOSED_LIB_DIR "libxposed_dalvik.so"
-#define XPOSED_LIB_ART           XPOSED_LIB_DIR "libxposed_art.so"
-#define XPOSED_JAR               "/system/framework/XposedBridge.jar"
-#define XPOSED_JAR_NEWVERSION    XPOSED_DIR "bin/XposedBridge.jar.newversion"
+#define XPOSED_LIB_DALVIK        XPOSED_LIB_DIR "libshadow_dalvik.so"
+#define XPOSED_LIB_ART           XPOSED_LIB_DIR "libshadow_art.so"
+#define XPOSED_JAR               "/system/framework/ShadowBridge.jar"
+#define XPOSED_JAR_NEWVERSION    XPOSED_DIR "bin/ShadowBridge.jar.newversion"
 #define XPOSED_LOAD_BLOCKER      XPOSED_DIR "conf/disabled"
 #define XPOSED_SAFEMODE_NODELAY  XPOSED_DIR "conf/safemode_nodelay"
 #define XPOSED_SAFEMODE_DISABLE  XPOSED_DIR "conf/safemode_disable"
 
-#define XPOSED_CLASS_DOTS_ZYGOTE "de.robv.android.xposed.XposedBridge"
-#define XPOSED_CLASS_DOTS_TOOLS  "de.robv.android.xposed.XposedBridge$ToolEntryPoint"
+#define XPOSED_CLASS_DOTS_ZYGOTE "com.bytefaker.android.shadow.ShadowBridge"
+#define XPOSED_CLASS_DOTS_TOOLS  "com.bytefaker.android.shadow.ShadowBridge$ToolEntryPoint"
 
 #if XPOSED_WITH_SELINUX
 #include <selinux/selinux.h>
